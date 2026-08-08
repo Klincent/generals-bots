2f2da83f422e10804663eb57a4c384b8e0844ba9
Klincent <juraj.fabo@gmail.com>
2026-08-08 13:52:45 +0200

    V2.7.5: switch wrapper to final source

--- competition/agents/juraj_cpp/main.cpp
+++ competition/agents/juraj_cpp/main.cpp
@@ -1,9 +1,9 @@
-// V2.7.3 source split into exact include chunks for repository transport.
-#include "v273_part01.inc"
-#include "v273_part02.inc"
-#include "v273_part03.inc"
-#include "v273_part04.inc"
-#include "v273_part05.inc"
-#include "v273_part06.inc"
-#include "v273_part07.inc"
-#include "v273_part08.inc"
+// V2.7.5 source split into exact include chunks for repository transport.
+#include "v275d_part01.inc"
+#include "v275d_part02.inc"
+#include "v275d_part03.inc"
+#include "v275d_part04.inc"
+#include "v275d_part05.inc"
+#include "v275d_part06.inc"
+#include "v275e_part07a.inc"
+#include "v275e_part07b.inc"
